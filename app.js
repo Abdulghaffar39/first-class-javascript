2f6bb2fd9c6b718c3f1b472f704a5b17c9a13502
Abdulghaffar39 <abdulghaffar15a@outlook.com>
2025-02-17 10:20:53 +0500

    console.log

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("Abdul Ghaffar")
+console.log("12313")
+console.log(12345)
+console.log(true)
+
+
+
+// commit in javascript
+
+// console.log("Abdul Ghaffar");
+// console.log("start");
+// console.log("file");
+// console.log("end");
+
+// ----------------------------------------------------------------------------------------
+//  -------Data type------
+
+// 1. string ("dopple qoutation year 2034") ('single quatation Ghaffar 530934')
+// 2. Number (12234, 123.523)
+// 3. Boolean (true or fales)
+
+// 4.Object
+
+
+console.log(typeof("sdlfos"))
+console.log(typeof("23424"))
+console.log(typeof(213049))
+console.log(typeof(true))
+
+console.log("20" + "20")
+console.log("Visual Studio Code")
+
+
+console.log(20 + 20)
+console.log(20 - 20)
+console.log(20 * 20)
+console.log(20 / 20)
+
+
+console.log(true + 20)
+console.log(true + false)
+console.log(true * false)
+console.log(true / false)
+
+
+
+
+// -----------------------------------------------------------------------------------------
